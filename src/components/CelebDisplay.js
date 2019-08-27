8200f9f9fa59685888ebc0115ad9fc8051d5297f
ren.meta@gmail.com <ren.meta@gmail.com>
2019-08-27 11:28:19 -0400

    onClick buttons logic for isDead working

--- src/components/CelebDisplay.js
+++ src/components/CelebDisplay.js
@@ -17,10 +17,20 @@ const CelebDisplay = props => {
   // };
   // console.log('rand', randomCeleb())
 
-  const rand = celebs[Math.floor(Math.random() * celebs.length)]
-  console.log("rand", rand);
+  const randomCeleb = celebs[Math.floor(Math.random() * celebs.length)]
+  console.log("randomCeleb", randomCeleb);
 
-  if (rand) console.log(rand.name)
+  if (randomCeleb) console.log(randomCeleb.name)
+
+  // const isDead = () => {
+  //   if (randomCeleb) {
+  //     return console.log(randomCeleb.isDead);
+  //   }
+  // };
+
+  const isDead = randomCeleb ? randomCeleb.isDead : null
+
+  // console.log('isDead', isDead);
 
   return (
     <Animated
@@ -29,14 +39,25 @@ const CelebDisplay = props => {
       isVisible={true}
     >
       <div className="celeb-container">
-        <h2>{rand ? rand.name : null}</h2>
+        <h2>{randomCeleb ? randomCeleb.name : null}</h2>
         {/* <img src={props.celeb.img} alt={props.celeb.name}/> */}
         <h4> short description? </h4>
-        <h4> birth year </h4>
-        <button>Alive!</button>
-        <Animated animationIn="shake" animationOut="fadeOut" isVisible={true}>
-          <button>Dead!</button>
-        </Animated>
+        <h4> {randomCeleb ? randomCeleb.dob : null} </h4>
+        <button
+          onClick={() => {
+            return console.log(!isDead);
+          }}
+        >
+          Alive!
+        </button>
+
+        <button
+          onClick={() => {
+            return console.log(isDead);
+          }}
+        >
+          Dead!
+        </button>
         <p>Current Score props.score</p>
       </div>
     </Animated>
