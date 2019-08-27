23a92f191e7ca824efe521ef3850f2e3f0fca315
JCRN <clemons.jason@gmail.com>
2019-08-27 16:03:18 -0700

    added semantic-ui-react tags to Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import {Animated} from "react-animated-css";
-
+import { Animated } from 'react-animated-css'
+import { Button } from 'semantic-ui-react'
 const Welcome = props => {
-console.log(props)
+  console.log(props)
 
-    return (
-        <div>
-        <h1>Welcome to the Celeb Dead or Alive Quiz</h1>
-        <h2>Test your celebrity knowledge</h2>
-        <p></p>
-        <button onClick={() => props.history.push("/game")}>Play Now</button>
-        </div>
-        
-        
-    )
+  return (
+    <div>
+      <h1>Welcome to the Celeb Dead or Alive Quiz</h1>
+      <h2>Test your celebrity knowledge</h2>
+      <p></p>
+      <Button onClick={() => props.history.push('/game')}>Play Now</button>
+    </div>
+  )
 }
 
 export default Welcome
