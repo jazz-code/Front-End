1265696ea07d3cbef2abeb7cb4facf2d632b1d82
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-08-27 09:29:13 -0700

    NavBar Feature Update.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 //
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
+  BrowserRouter as Router,
   Route,
   Link,
-  Redirect
+  Redirect,
 } from 'react-router-dom'
 
 import CelebDisplay from './components/CelebDisplay'
 import Welcome from './components/Welcome'
-import NavBar from './components/NavBar'
+
+import { ScoreProvider } from './contexts/ScoreContext'
+import { UserProvider } from './contexts/UserContext'
 
 function App() {
+  const [celebs, setCelebs] = useState([])
+
+  const [celebrity, setCelebrity] = useState({})
+
+  const [score, setScore] = useState({
+    score: null,
+    totalScore: null
+  })
+
+  const [user, setUser] = useState({
+    id: null,
+    name: '',
+    username: '',
+    password: '',
+    points: null
+  })
+
   return (
-    
+    <Router>
       <div className="App">
-        <NavBar />
-        <Route exact path="/" component={Welcome} />
-          <h1>Celeb Dead Or Alive</h1>
-        <Route exact path="/game" component={CelebDisplay} />
+        <UserProvider value={user}>
+          <ScoreProvider value={score}>
+            <Route exact path="/" component={Welcome} />
+            <h1>Celeb Dead Or Alive</h1>
+            <Route exact path="/game" component={CelebDisplay} />
+          </ScoreProvider>
+        </UserProvider>
       </div>
-    
+    </Router>
   )
 }
 
