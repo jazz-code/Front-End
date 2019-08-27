9bd73cc272167a18ec645895e1771be4d3c6c96d
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-08-27 09:06:53 -0700

    Added the NavLink for Login and Signup.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,8 @@ export default function NavBar() {
     return(
         <div className='navBar'>
             <NavLink to='/'>Celebrity Dead Or Alive</NavLink>
+            <NavLink to='/login'>Login</NavLink>
+            <NavLink to='/signup'>Signup</NavLink>
         </div>
     );
 }
