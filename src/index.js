a085254640448812cd7e146bb9d1916267dee69a
Shawn McManus <xsvfan76@gmail.com>
2019-08-28 11:31:25 -0600

    almost have login done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
-import 'semantic-ui-css/semantic.min.css'
-import './styling/main.scss'
-import './index.css'
-import App from './App'
+import 'semantic-ui-css/semantic.min.css';
+import './styling/main.scss';
+import './index.css';
+import App from './App';
 
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
-    <Router>
-        <App/>        
-    </Router>,
-    document.getElementById('root')
+  <Router>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  </Router>,
+  document.getElementById('root'),
 );
