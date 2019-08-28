4921d24392c559c122404fdfd71375fdc71ac733
Brandon-Harris <brandonharris177@gmail.com>
2019-08-28 09:12:49 -0600

    second-update

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -44,39 +44,39 @@ const Signup = () => {
 
 
     return(
-      <div className = 'signup-container'>
-        <Card className = 'signup-card'>
-            <form className='signup-form'>
-                <h1>Signup:</h1>
-                <label htmlFor = 'name' className='signup-label'/>
-                <input 
-                    name = 'full name'
-                    placeholder = 'Full Name'
-                    type = 'text'
-                    // value = {login.username} //this value is updated by the changeHandler
-                    // onChange= {changeHandler}/>
-                />
-                <label htmlFor = 'username' className='signup-label'/>
-                <input 
-                    name = 'username'
-                    placeholder = 'Username'
-                    type = 'text'
-                    // value = {login.username} //this value is updated by the changeHandler
-                    // onChange= {changeHandler}/>
-                />
-                <label htmlFor = 'password' className='login-label'/>
-                <input 
-                    name = 'password'
-                    placeholder = 'Password'
-                    type = 'password'
-                    // value  = {login.password} //this value is updated by the changeHandler
-                    // onChange = {changeHandler}/>
-                />
-                    <br></br>
-                <Button type='submit' inverted color='blue' className= "signup-button">Submit</Button>
-            </form>
-        </Card>
-      </div>
+    <div className = 'signup-container'>
+      <Card className = 'signup-card'>
+          <form className='signup-form'>
+              <h1>Signup:</h1>
+              <label htmlFor = 'name' className='signup-label'/>
+              <input 
+                  name = 'full name'
+                  placeholder = 'Full Name'
+                  type = 'text'
+                  // value = {login.username} //this value is updated by the changeHandler
+                  // onChange= {changeHandler}/>
+              />
+              <label htmlFor = 'username' className='signup-label'/>
+              <input 
+                  name = 'username'
+                  placeholder = 'Username'
+                  type = 'text'
+                  // value = {login.username} //this value is updated by the changeHandler
+                  // onChange= {changeHandler}/>
+              />
+              <label htmlFor = 'password' className='login-label'/>
+              <input 
+                  name = 'password'
+                  placeholder = 'Password'
+                  type = 'password'
+                  // value  = {login.password} //this value is updated by the changeHandler
+                  // onChange = {changeHandler}/>
+              />
+                  <br></br>
+              <Button type='submit' inverted color='blue' className= "signup-button">Submit</Button>
+          </form>
+      </Card>
+    </div>
     )
 
 }
