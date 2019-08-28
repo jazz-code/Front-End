23955a3030a22bf0f5b573726e23aa957025b942
ren.meta@gmail.com <ren.meta@gmail.com>
2019-08-28 12:17:52 -0400

    merging

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Animated } from 'react-animated-css'
-import { Button } from 'semantic-ui-react'
-
+import React from "react";
+import { Animated } from "react-animated-css";
+import { Button } from "semantic-ui-react";
+import UnregisteredPlayerModal from "./UnregisteredPlayerModal";
 const Welcome = props => {
-  console.log(props)
-
-
-    return (
-        <div>
-        <h1>Welcome to the Celeb Dead or Alive Quiz</h1>
-        <h2>Test your celebrity knowledge</h2>
-        <p></p>
-        <Button onClick={() => props.history.push('/game')}>Play Now</Button>
-        </div>
-        
-        
-    )
-
-}
-
-export default Welcome
+  const { history, score } = props
+  console.log('welcome props: ', props)
+   return (
+     <div>
+       <h1>Welcome to the Celeb Dead or Alive Quiz</h1>
+       <h2>Test your celebrity knowledge</h2>
+       <p></p>
+      <Button onClick={() => props.history.push('/game')}>Play Now</Button>
+       {score === 5 ? <UnregisteredPlayerModal score={score} /> : null}
+     </div>
+   );
+};
+export default Welcome;
