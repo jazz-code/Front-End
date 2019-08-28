c9f723906c843e33d21dd0b5e358044bf04a452b
Jason Clemons <clemons.jason@gmail.com>
2019-08-28 20:29:36 +0000

    Revert "Get sign up to work feature"

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,82 +2,72 @@ import React, { useState, useEffect } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import axios from 'axios';
 
-export default function Signup(props) {
-  const [user, setUsers] = useState({
-    name: '',
-    username: '',
-    password: '',
-    points: null,
+const Signup = () => {
+  const [signup, setSignup] = useState({
+    name: "",
+    username: "",
+    password: ""
   });
 
-  console.log('user', user);
-
   const changeHandler = event => {
-    const updatedUser = { ...user, [event.target.name]: event.target.value };
+    setSignup({ ...signup, [event.target.name]: event.target.value });
+    console.log(signup);
+    //...login gives us the original state of login whaever it last was {...$anything} (in this case {...login}) then we add whatever the value is (event.target.value) to whatever the changed feild it corresponds to (event.target.name)
+  }
 
-    setUsers(updatedUser);
-  };
 
-  const handleSubmit = event => {
+  const submitForm = event => {
     event.preventDefault();
+    console.log(`name: ${signup.name}`, `username: ${signup.username}`, `password: ${signup.password}`)
     axios
-      .post('https://bw-celeb-dead-app.herokuapp.com/auth/register', user)
-      .then(results => {
-        console.log(results.data);
-        return props.history.push('/game');
+      .post("https://bw-celeb-dead-app.herokuapp.com/auth/signup", {
+        name: `${signup.name}`,
+        username: `${signup.username}`,
+        password: `${signup.password}`
+      })
+      .then(response => {
+        console.log(response.data);
+        // .props.history.data
       })
       .catch(error => {
-        console.log('O no there is an error!', error);
-
+        console.error("Signup.js: Server Error ", error);
       });
   };
 
   return (
-    <Card className='signup-card'>
-      <form onSubmit={handleSubmit} className='signup-form'>
-        <h1>Signup:</h1>
-        <label htmlFor='name' className='signup-label' />
-        <input
-          name='name'
-          placeholder='Full Name'
-          type='text'
-          value={user.name} //this value is updated by the changeHandler
-          onChange={changeHandler}
-        />
-        />
-        <label htmlFor='username' className='signup-label' />
-        <input
-          name='username'
-          placeholder='Username'
-          type='text'
-          value={user.username} //this value is updated by the changeHandler
-          onChange={changeHandler}
-        />
-        />
-        <label htmlFor='password' className='login-label' />
-        <input
-          name='password'
-          placeholder='Password'
-          type='password'
-          value={user.password} //this value is updated by the changeHandler
-          onChange={changeHandler}
-        />
-        />
-        <label htmlFor='username' className='signup-label' />
-        <input
-          name='points'
-          placeholder='enter points'
-          type='number'
-          value={user.points} //this value is updated by the changeHandler
-          onChange={changeHandler}
-        />
-        />
-        <br></br>
-        <Button type='submit' inverted color='blue' className='signup-button'>
-          Submit
-        </Button>
-      </form>
-    </Card>
+    <div className="signup-container">
+      <Card className="signup-card">
+        <form onSubmit={submitForm} className="signup-form">
+          <h1>Signup:</h1>
+          <label htmlFor="name" className="signup-label" />
+          <input
+            name="name"
+            placeholder="Name"
+            type="text"
+            value={signup.name} //this value is updated by the changeHandler
+            onChange={changeHandler}
+          />
+          <label htmlFor="username" className="signup-label" />
+          <input
+            name="username"
+            placeholder="Username"
+            type="text"
+            value={signup.username} //this value is updated by the changeHandler
+            onChange={changeHandler}
+          />
+          <label htmlFor="password" className="signup-label" />
+          <input
+            name="password"
+            placeholder="Password"
+            type="password"
+            value={signup.password} //this value is updated by the changeHandler
+            onChange={changeHandler}
+          />
+          <br></br>
+          <Button type='submit' inverted color='blue' className= 'signup-button'>Submit</Button>
+        </form>
+      </Card>
+    </div>
   );
 }
 
