392ef8aa577b9537aace8c8b70b1b0ed8c923298
JCRN <clemons.jason@gmail.com>
2019-08-28 09:59:33 -0700

    update

--- src/components/RegisteredPlayerModal.js
+++ src/components/RegisteredPlayerModal.js
@@ -13,6 +13,12 @@ const RegisteredPlayerModal = ({ score }) => (
       <Modal.Description>
         <Header>CONGRATULATIONS!</Header>
         <p>You REALLY Seem to Know A LOT About Dead Celebrities...</p>
+        <p>
+          We worked our voodoo and tallied your score, compared it to all of
+          your challengers scores, and you're ranking in the top
+          {/*spoof leader board, (everyone in the top 1% for feelz?) random percentage*/}{' '}
+          of celebrity know-it-alls!
+        </p>
       </Modal.Description>
     </Modal.Content>
   </Modal>
