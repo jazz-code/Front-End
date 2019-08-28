02f2ce20ed041ffe4ce878899a46cd2489659ac5
ren.meta@gmail.com <ren.meta@gmail.com>
2019-08-28 11:46:22 -0400

    stable version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Link,
-<<<<<<< HEAD
   Redirect
 } from "react-router-dom";
 
@@ -11,47 +10,29 @@ import Celeb from "./components/Celeb";
 import CelebDisplay from "./components/CelebDisplay";
 import Welcome from "./components/Welcome";
 import Signup from "./components/Signup";
-import Login from './components/Login';
+import Login from "./components/Login";
 
 import { ScoreProvider } from "./contexts/ScoreContext";
 import { UserProvider } from "./contexts/UserContext";
 import NavBar from "./components/NavBar";
 
-=======
-  Redirect,
-
-} from 'react-router-dom'
-
-
-import Celeb from './components/Celeb';
-import CelebDisplay from './components/CelebDisplay'
-import Welcome from './components/Welcome'
-import Signup from './components/Signup'
-import Login from './components/Login'
-
-import { ScoreProvider } from './contexts/ScoreContext'
-import { UserProvider } from './contexts/UserContext'
-import NavBar from './components/NavBar';
-
-
->>>>>>> 4bbe18016d44cd05df55a513f5159aee472f03dc
 function App() {
-  const [celebs, setCelebs] = useState([])
+  const [celebs, setCelebs] = useState([]);
 
-  const [celebrity, setCelebrity] = useState({})
+  const [celebrity, setCelebrity] = useState({});
 
   const [score, setScore] = useState({
     score: null,
-    totalScore: null
-  })
+    totalScore: null,
+  });
 
   const [user, setUser] = useState({
     id: null,
-    name: '',
-    username: '',
-    password: '',
-    points: null
-  })
+    name: "",
+    username: "",
+    password: "",
+    points: null,
+  });
 
   return (
     <Router>
@@ -59,23 +40,16 @@ function App() {
         <NavBar />
         <UserProvider value={user}>
           <ScoreProvider value={score}>
-<<<<<<< HEAD
             <NavBar />
-=======
->>>>>>> 4bbe18016d44cd05df55a513f5159aee472f03dc
             <Route exact path="/" component={Welcome} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/game" component={CelebDisplay} />
-            <Route exact path= "/signup" component={Signup} />
+            <Route exact path="/signup" component={Signup} />
           </ScoreProvider>
         </UserProvider>
       </div>
     </Router>
-  );
+  )
 }
 
-
-export default App
-
-     
-
+export default App;
--- src/components/CelebDisplay.js
+++ src/components/CelebDisplay.js
@@ -1,47 +1,38 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import { Animated } from 'react-animated-css';
-import { Card, Icon, Image, Button } from 'semantic-ui-react';
-import axios from 'axios';
-=======
 import React, { useState, useEffect } from "react";
 import { Animated } from "react-animated-css";
 import { Card, Icon, Image, Button } from "semantic-ui-react";
-
 import axios from "axios";
->>>>>>> 4bbe18016d44cd05df55a513f5159aee472f03dc
 
-import '../styling/components/celebdisplay.scss';
-
+import "../styling/components/celebdisplay.scss";
 
 const CelebDisplay = props => {
-  const [celebs, setCelebs] = useState([])
-  const [currentScore, setCurrentScore] = useState(0)
-  const [width, setWidth] = useState(0)
+  const [celebs, setCelebs] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [width, setWidth] = useState(0);
 
   useEffect(() => {
     axios
-      .get('https://bw-celeb-dead-app.herokuapp.com/celebs')
+      .get("https://bw-celeb-dead-app.herokuapp.com/celebs")
       .then(res => setCelebs(res.data))
-      .catch(err => err.response)
-  }, []);
+      .catch(err => err.response);
+  }, [])
 
   const nextCeleb = () => {
-    let i = 0
-    i = randomCeleb + i // increase random
-    i = i % celebs.length // if we've gone too high, start from `0` again
-    return celebs[i] // give us back the celeb of where we are now
-  };
+    let i = 0;
+    i = randomCeleb + i; // increase random
+    i = i % celebs.length; // if we've gone too high, start from `0` again
+    return celebs[i]; // give us back the celeb of where we are now
+  }
 
-  const randomCeleb = celebs[Math.floor(Math.random() * celebs.length)];
-  console.log('randomCeleb', randomCeleb)
+  const randomCeleb = celebs[Math.floor(Math.random() * celebs.length)]
+  console.log("randomCeleb", randomCeleb);
 
   // if (randomCeleb) console.log(randomCeleb.name);
 
-  const isDead = randomCeleb ? randomCeleb.isDead : null;
+  const isDead = randomCeleb ? randomCeleb.isDead : null
 
   if (currentScore === 5) {
-    props.history.push("/login")
+    props.history.push('/login');
   }
 
   // function addWidth() {
@@ -75,8 +66,8 @@ const CelebDisplay = props => {
           className="btn-alive ui labeled icon button"
           onClick={() => {
             if (!isDead) {
-              alert("Correct")
-              setCurrentScore(currentScore + 1)
+              alert('Correct');
+              setCurrentScore(currentScore + 1);
 
               return (
                 <Animated
@@ -84,12 +75,12 @@ const CelebDisplay = props => {
                   animationOut="fadeOut"
                   isVisible={true}
                 >
-                  {props.history.push('/game')}
+                  {props.history.push("/game")}
                 </Animated>
-              )
+              );
             } else {
-              alert("Wrong");
-              props.history.push("/game");
+              alert('Wrong')
+              props.history.push('/game')
             }
           }}
         >
@@ -100,12 +91,12 @@ const CelebDisplay = props => {
           className="btn-dead ui labeled icon button"
           onClick={() => {
             if (isDead) {
-              alert("Correct");
-              props.history.push("/game");
-              setCurrentScore(currentScore + 1)
+              alert('Correct')
+              props.history.push('/game')
+              setCurrentScore(currentScore + 1);
             } else {
-              alert("Wrong");
-              props.history.push("/game");
+              alert('Wrong')
+              props.history.push('/game')
             }
           }}
         >
@@ -117,7 +108,7 @@ const CelebDisplay = props => {
         <div className="score percent">Current Score: {currentScore}</div>
       </div>
     </Animated>
-  );
-}
+  )
+};
 
-export default CelebDisplay;
+export default CelebDisplay
