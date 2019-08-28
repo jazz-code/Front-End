ce1f257aea36b7af1b6ae91a29cdba5b4a87a7eb
JCRN <clemons.jason@gmail.com>
2019-08-28 09:46:42 -0700

    update

--- src/components/UnregisteredPlayerModal.js
+++ src/components/UnregisteredPlayerModal.js
@@ -13,8 +13,18 @@ const UnRegisteredPlayerModal = ({ score }) => (
       <Modal.Description>
         <Header>CONGRATULATIONS!</Header>
         <p>You REALLY Seem to Know A LOT About Dead Celebrities...</p>
+        <p>
+          That was a lot of fun right? Want to let everyone know if they are
+          ever trapped in a room with a serial killer and the only way to make
+          it out alive is to correctly guess whether or not 10 random
+          celebrities are DEAD or ALIVE? Create an account so your amazing score
+          persists!
+        </p>
       </Modal.Description>
     </Modal.Content>
+    <Button primary icon>
+      Create an Account <Icon name="right chevron" />
+    </Button>
   </Modal>
 )
 
