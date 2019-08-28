169ff350869591def48c333d104d413cfdb450b3
Shawn McManus <xsvfan76@gmail.com>
2019-08-28 14:08:34 -0600

    removed-login-button-from-welcome-page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import {Animated} from "react-animated-css";
+import React from 'react';
+import { Animated } from 'react-animated-css';
 
 const Welcome = props => {
-console.log(props)
+  console.log(props);
 
-    return (
-        <div>
-        <button onClick onClick={() => props.history.push("/login")}>Login</button>
-        <h1>Welcome to the Celeb Dead or Alive Quiz</h1>
-        <h2>Test your celebrity knowledge</h2>
-        <p></p>
-        <button onClick={() => props.history.push("/game")}>Play Now</button>
-        </div>
-        
-        
-    )
-}
+  return (
+    <div>
+      <h1>Welcome to the Celeb Dead or Alive Quiz</h1>
+      <h2>Test your celebrity knowledge</h2>
+      <p></p>
+      <button onClick={() => props.history.push('/game')}>Play Now</button>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
