8903f24d6ee188476b3af314753c4c296ffc5a5a
Brandon-Harris <brandonharris177@gmail.com>
2019-08-29 13:16:03 -0600

    stuck

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,14 +6,41 @@ function Timer() {
       setInterval(() => {
         setTime((newTime) => newTime-1)
       }, 1000)
-      if ("newTime" === "0") {
-        this.props.history.push('/game')
-        setTime(3)
-      }
+    //   if (`${}` === `3`) {
+    //   }
+    //   else {
+    //       console.log(`error`)
+    //   }
     }, [])
     return (
         <div className="timer">{time}</div>
     )
 }
+
+// function Timer() {
+//     const [time, setTime]=useState(3);
+//     let ThisArray = []
+//     // const [newTime, setNewTime]=useState("")
+//     useEffect(() => {
+//       setInterval(() => {
+//         setTime((newTime) => newTime-1)
+//         ThisArray.push(1)
+//         console.log(`${ThisArray}`)
+//       }, 1000)
+//       if (`${ThisArray}` === `1, 1, 1`) {
+//           console.log(`its working`)
+//         this.props.history.push('/game');
+//         ThisArray.shift();
+//         ThisArray.shift();
+//         ThisArray.shift();
+//       }
+//       else {
+//           console.log(`at least you are seeing this`)
+//       }
+//     }, [])
+//     return (
+//         <div className="timer">{time}</div>
+//     )
+// }
 
 export default Timer
